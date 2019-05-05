cee891861d00985739ec4f54e1070a43a8f1dc31
Shamil Ganeev <sh2@bk.ru>
2019-05-05 23:18:40 +0300

    Реализовано ДЗ2
    доработан текст сообщений в main

--- src/ru/geekbrains/JavaCore2/lesson2/add/DayOfWeekMain.java
+++ src/ru/geekbrains/JavaCore2/lesson2/add/DayOfWeekMain.java
@@ -3,22 +3,19 @@ package ru.geekbrains.JavaCore2.lesson2.add;
 public class DayOfWeekMain {
     public static void main(final String[] args) {
         DayOfWeek day;
-        int i; // порядковый номер дны недели
+        int i; // порядковый номер дня недели при счете с нуля
         int n; // количество элементов в перечислении
         DayOfWeek[] daysOfWeek; //массив из значений перечисления
 
-        day = DayOfWeek.SUNDAY ;// здесь можно будет интерактивно подавать на вход день недели
+        day = DayOfWeek.THURSDAY ;// здесь можно будет интерактивно подавать на вход день недели
         i = day.ordinal();
         daysOfWeek = DayOfWeek.values();
         n = daysOfWeek.length;
 
-        System.out.println("порядковый номер " + day.getRussianWeekDayName() + " есть " + i);
-        System.out.println("количество элементов в перечислении " + n);
-        System.out.println("в начале рабочего дня в "+ day.getRussianWeekDayName() + " до конца рабочей недели остаётся " + day.getRemainingWeekWorkingHours() + " часов");
-//TODO
-/*        сделать цикл for each для списка енума, который будет бежать от текущей позиции дня недели до конца недели и считать часы, оставшиеся до конца рабочей недели
+        System.out.println("порядковый номер дня недели " + day.getRussianWeekDayName() + " есть " + (i+1));
+        System.out.println("количество дней в неделе " + n);
+        System.out.println("в начале дня в "+ day.getRussianWeekDayName() + " до конца рабочей недели остаётся " + day.getRemainingWeekWorkingHours() + " рабочих часов");
 
- */
     }
 
 }
